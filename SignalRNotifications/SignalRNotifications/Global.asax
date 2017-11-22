f3f9be34ceffda0647c8775be57de8569adc8767
guptaisha5484 <guptaisha5484@gmail.com>
2017-11-22 21:43:56 +0530

    SignalR Sample

---
+++ SignalRNotifications/SignalRNotifications/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRTest.Global" Language="C#" %>
---
+++ SignalRNotifications/SignalRNotifications/Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Builder;
+using Owin;
+
+namespace SignalRTest
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Register the default hubs route: ~/signalr/hubs
+            //RouteTable.Routes.MapHubs();
+            //IAppBuilder appBuilder = new AppBuilder();
+            //appBuilder.MapSignalR();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

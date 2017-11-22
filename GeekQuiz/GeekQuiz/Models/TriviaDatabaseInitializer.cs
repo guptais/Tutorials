678bcbb40fb3a23e1d7d1b22c57efb9e3ade3214
guptaisha5484 <guptaisha5484@gmail.com>
2017-11-22 21:53:27 +0530

    Web API practice

---
+++ GeekQuiz/GeekQuiz/Models/TriviaDatabaseInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using GeekQuiz.Models;
+
+namespace GeekQuiz
+{
+    public class TriviaDatabaseInitializer<T> : IDatabaseInitializer<T> where T: TriviaDbContext
+    {
+        public void InitializeDatabase(T context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeekQuiz/GeekQuiz/Models/TriviaDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using DbContext = System.Data.Entity.DbContext;
+
+namespace GeekQuiz.Models
+{
+    public class TriviaDbContext : DbContext
+    {
+        private static bool _created = false;
+
+        public TriviaDbContext()
+        {
+            if (!_created)
+            {
+                _created = true;
+                Database.EnsureCreated();
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TriviaOption>()
+                .HasKey(o => new { o.QuestionId, o.Id });
+
+            builder.Entity<TriviaAnswer>()
+                .HasOne(a => a.TriviaOption)
+                .WithMany()
+                .HasForeignKey(a => new { a.QuestionId, a.OptionId });
+
+            builder.Entity<TriviaQuestion>()
+                .HasMany(q => q.Options)
+                .WithOne(o => o.TriviaQuestion);
+        }
+        
+        public System.Data.Entity.DbSet<TriviaQuestion> TriviaQuestions { get; set; }
+
+        public System.Data.Entity.DbSet<TriviaOption> TriviaOptions { get; set; }
+
+        public System.Data.Entity.DbSet<TriviaAnswer> TriviaAnswers { get; set; }
+    }
+}
